57cd53511db74d1345a362d3091cad56085a0d94
MAizada <aizadamukhamedgaliyeva@gmail.com>
2024-03-25 20:01:45 +0500

    Initial Commit

---
+++ TravellingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravellingApp
+//
+//  Created by Aizada on 25.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
